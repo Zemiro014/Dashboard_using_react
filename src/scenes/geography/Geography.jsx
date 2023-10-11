13e0ea7873f9f568bf1c2721e5a7c7f0b5adaf24
ntconsult.jeronimo <ntconsult.jeronimo@agi.com.br>
2023-10-11 12:19:21 -0300

    Built the Line and Geography  Charts using the nivo

--- src/scenes/geography/Geography.jsx
+++ src/scenes/geography/Geography.jsx
@@ -1,8 +1,22 @@
+import { Box, useTheme } from '@mui/material'
 import React from 'react'
+import Header from '../../components/Header'
+import GeographyChart from '../../components/GeographyChart'
+import { mockGeographyData as data } from '../../data/mockData'
+import { geoFeatures } from '../../data/mockGeoFeatures'
+import { tokens } from '../../theme'
 
 const Geography = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+
   return (
-    <div>Geography</div>
+    <Box m="20px">
+      <Header title="Geography Chart" subtitle="Simple Geography Chart" />
+      <Box height="75vh" border={`1px solid  ${colors.grey[100]}`} borderRadius="4px">
+        <GeographyChart data={data} features={geoFeatures.features} />
+      </Box>
+    </Box>
   )
 }
 
--- src/scenes/line/Line.jsx
+++ src/scenes/line/Line.jsx
@@ -1,8 +1,17 @@
+import { Box } from '@mui/material'
 import React from 'react'
+import Header from '../../components/Header'
+import LineChart from '../../components/LineChart'
+import { mockLineData as data } from '../../data/mockData'
 
 const Line = () => {
   return (
-    <div>Line</div>
+    <Box m="20px">
+    <Header title="Pie Chart" subtitle="Simple Pie Chart" />
+    <Box height="75vh">
+      <LineChart data={data} />
+    </Box>
+  </Box>
   )
 }
 
